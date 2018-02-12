81051189dc7b1e724123ccd70b8073c189c5c327
“michal-adamczyk8 <adamczyk888@gmail.com>
2018-02-12 16:37:53 +0100

    Zadanie 10.1

---
+++ src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
@@ -0,0 +1,19 @@
+package com.kodilla.spring.forum;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ForumUserTestSuite {
+    @Test
+    public void testGetUsername() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        ForumUser forumUser = context.getBean(ForumUser.class);
+        //When
+        String name = forumUser.getUserName();
+        //Then
+        Assert.assertEquals("John Smith", name);
+    }
+}
